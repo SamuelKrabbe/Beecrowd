408ae71d448c8a0c890946953f86c7e41804cf38
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-05-25 17:00:27 -0400

    .

---
+++ iniciante/prob_2286.py
@@ -0,0 +1,25 @@
+while True:
+    n = int(input())
+
+    if n == 0:
+        break
+
+    x = 1
+    ganhador = []
+
+    nome1 = input()
+    nome2 = input()
+
+    for i in range(n):
+        j1, j2 = map(int, input().split())
+
+        if (j1 + j2) % 2 == 0:
+            ganhador.append(nome1)
+        else:
+            ganhador.append(nome2)
+
+    print("Teste {}".format(x))
+    x += 1
+
+    for j in range(len(ganhador)):
+        print(ganhador[j])

756e3704760dd813e74b2b07bdd01f73c23f24b1
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-07-01 12:51:10 -0400

    removing iniciante folder

---
+++ prob_1255.py
@@ -0,0 +1,67 @@
+n = int(input())
+alfabeto = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+
+for i in range(n):
+    string = input()
+    new_string = ""
+
+    """
+    Organizando os caracteres
+    """
+    string = string.strip()
+    string = string.lower()
+
+    for caractere in string:
+        if caractere in alfabeto:
+            new_string += caractere
+
+    """
+    Colocando a frequência de cada letra em uma lista e colocando a com maior frequencia em um variável
+    """
+    lista_frequencia = []
+
+    for letra in new_string:
+        frequencia_letra = new_string.count(letra)
+        lista_frequencia.append(frequencia_letra)
+
+    maior_frequencia = max(lista_frequencia)
+
+    """
+    Verificando se existem letras com a mesma frequencia e imprimindo elas
+    """
+    maior_frequencia_letra = ""
+
+    for j in range(len(lista_frequencia)):
+        if lista_frequencia[j] == maior_frequencia:
+            if new_string[j] not in maior_frequencia_letra:
+                maior_frequencia_letra += new_string[j]
+
+    print(maior_frequencia_letra)

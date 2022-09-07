8360e4eac05d2b6693bdcf53d691f891376b2728
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-09-07 17:20:37 -0400

    .

---
+++ c/prob_1182.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int column;
+    char operation;
+    float matriz[12][12], soma = 0.0, media;
+
+    scanf("%d", &column);
+    scanf("%c", &operation);
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%f", matriz[i][j]);
+        }
+    }
+
+    for (int k = 0; k < 12; k++)
+    {
+        soma += matriz[k][column];
+    }
+
+    if (operation == 'M')
+    {
+        media = soma / 12.0;
+        printf("%d", media);
+    }
+    else
+    {
+        printf("%f", soma);
+    }
+    return 0;
+}

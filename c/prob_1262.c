5718513f9e4053bb417a0bd4e811d8202c999f51
Samuel Krabbe <101955583+SamuelKrabbe@users.noreply.github.com>
2022-09-08 14:07:51 -0400

    Update prob_1262.c
    otimização segundo o que o professor falou

--- c/prob_1262.c
+++ c/prob_1262.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #define LENGHT 51
 
-int main(void)
+int main()
 {
-    char track[LENGHT], lastChar = 'O';
-    int process, machineCycles = 0, countForR = 0, arrayLenght = 0;
-
-    while (scanf("%50s", track) != EOF)
+    char track[LENGHT];
+    int process;
+    
+    while (scanf("%s", track) != EOF)
     {
         scanf("%d", &process);
+
+        char lastChar = 'O';
+        int machineCycles = 0, countForR = 0, arrayLenght = 0;
+
 
         while (track[arrayLenght] != '\0')
         {
@@ -26,49 +30,30 @@ int main(void)
 
             if (countForR < process)
             {
-                if (lastChar = 'R')
+                if (track[i] == 'W')
                 {
-                    if (track[i] == 'W')
-                    {
-                        machineCycles++;
-                        lastChar = track[i];
-                        countForR = 0;
-                    }
-                    else
-                    {
-                        if (track[i] == 'R')
-                        {
-                            lastChar = track[i];
-                            countForR++;
-                        }
-                    }
+                    machineCycles++;
+                    lastChar = track[i];
+                    countForR = 0;
                 }
                 else
                 {
-                    if (track[i] == 'W')
+                    if (track[i] == 'R' && lastChar != 'R')
                     {
                         machineCycles++;
                         lastChar = track[i];
-                        countForR = 0;
+                        countForR++;
                     }
                     else
                     {
-                        if (track[i] == 'R')
-                        {
-                            machineCycles++;
-                            lastChar = track[i];
-                            countForR++;
-                        }
+                        lastChar = track[i];
+                        countForR++;
                     }
                 }
+                
             }
         }
         printf("%d\n", machineCycles);
-
-        machineCycles = 0;
-        countForR = 0;
-        lastChar = 'O';
-        arrayLenght = 0;
     }
     return 0;
 }

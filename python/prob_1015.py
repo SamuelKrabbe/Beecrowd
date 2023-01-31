737f16bd3606cae76fe4eb5082b528fc434d3ddb
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-01-30 21:28:56 -0400

    .

--- python/prob_1015.py
+++ python/prob_1015.py
@@ -1,8 +1,9 @@
+import math
+
 p1 = input().split()
 p2 = input().split()
 x1, y1 = p1
 x2, y2 = p2
-import math
 
 dist_betw = math.sqrt(((float(x2) - float(x1)) ** 2) + ((float(y2) - float(y1)) ** 2))
 

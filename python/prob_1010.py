ce2fe3a1fa19310d6e1a717bcd6070f319d903cd
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-01-30 21:28:17 -0400

    .

--- python/prob_1010.py
+++ python/prob_1010.py
@@ -1,7 +1,5 @@
-lista1 = input().split()
-lista2 = input().split()
-code1, num_unit1, price_unit1 = lista1
-code2, num_unit2, price_unit2 = lista2
+code1, num_unit1, price_unit1 = input().split()
+code2, num_unit2, price_unit2 = input().split()
 total = (int(num_unit1) * float(price_unit1)) + (int(num_unit2) * float(price_unit2))
 
 print(f"VALOR A PAGAR: R$ {total:.2f}")

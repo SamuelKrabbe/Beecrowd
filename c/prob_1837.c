e1e2b9d7bced771aa65631134d4bccbbad3e9174
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-01-31 22:42:05 -0400

    .

--- c/prob_1837.c
+++ c/prob_1837.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <time.h>
+
+float timeDiff(struct timeval *start, struct timeval *end)
+{
+    return ((end->tv_sec - start->tv_sec) + 1E-6 * (end->tv_usec - start->tv_usec));
+}
 
 int main()
 {
     int numerador, denominador, quociente, resto, resultado;
+    double tempoDeExecucao;
+    struct timeval start, end;
 
+    gettimeofday(&start, NULL);
     scanf("%d %d", &numerador, &denominador);
 
     if (numerador > 0)
@@ -26,5 +37,11 @@ int main()
     }
     
     printf("%d %d\n", quociente, resto);
+
+    gettimeofday(&end, NULL);
+    tempoDeExecucao = timeDiff(&start, &end);
+    
+    printf("%.8f\n", tempoDeExecucao);
+
     return 0;
 }

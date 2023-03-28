e3d0ba8af738ae0a09ca1182d454a007545812d8
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-03-28 17:01:55 -0400

    problema 1435 ta foda, pedir ajuda para entender!!

--- java/prob_1435.java
+++ java/prob_1435.java
@@ -4,13 +4,33 @@ public class prob_1435 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int tamanhoMat;
+    	int tamanhoMat = sc.nextInt();
+    	int aux;
 
-        do {
+    	while (tamanhoMat != 0) {
+    		for (int coluna = 1; coluna <= tamanhoMat; coluna++) {
+    			for (int linha = 1 ; linha <= tamanhoMat; linha++) {
+    				aux = coluna;
+
+                    if (linha < aux) 
+                        aux = linha;
+                    if (tamanhoMat - coluna + 1 < aux) 
+                        aux = tamanhoMat - coluna + 1;
+                    if (tamanhoMat - linha + 1 < aux) 
+                        aux = tamanhoMat - linha + 1;
+                    
+                    System.out.printf("%3d", aux);
+
+                    if (linha < tamanhoMat) 
+                        System.out.print(" ");
+                    else 
+                        System.out.print("\n");
+                }
+            }
+            System.out.print("\n");
             tamanhoMat = sc.nextInt();
-
-            
-        }while (tamanhoMat > 0);
+        }
+    
 
         sc.close();
     }
